221845bf3c0a16c7d40a4846d652d0245835283e
HanMeow <HanMeow@users.noreply.github.com>
2020-03-29 00:00:07 +0800

    'deploy'

--- service-worker.js
+++ service-worker.js
@@ -15,7 +15,7 @@ importScripts("/baccarat-20200208/worker/workbox-v4.3.1/workbox-sw.js");
 workbox.setConfig({modulePathPrefix: "/baccarat-20200208/worker/workbox-v4.3.1"});
 
 importScripts(
-  "/baccarat-20200208/worker/precache-manifest.de23a0369fc2a4653c85ac69689bac2b.js"
+  "/baccarat-20200208/worker/precache-manifest.b40b5f578c0c41c34f05808610728a97.js"
 );
 
 workbox.core.skipWaiting();
